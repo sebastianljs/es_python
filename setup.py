09dcef4e5c7f2643471d2a45a51796aa022eadbe
Sebastian Lin <sebastianljs@gmail.com>
2018-04-24 14:56:15 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+def main():
+    setup(
+        name="es-python",
+        version="1.0.0",
+        author="Sebastian Lin",
+        author_email="sebastianljs@gmail.com",
+        description="ES python exploration",
+        packages=["es_python"]
+    )
+
+
+if __name__ == "__main__":
+    main()
